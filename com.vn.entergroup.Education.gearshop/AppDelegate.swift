98b823f46dca9e83039d6fa5eebc3fdc64667bef
huyparody <m30d3n2010@gmail.com>
2018-09-20 16:19:36 +0700

    Update icons and launchscreen

--- com.vn.entergroup.Education.gearshop/AppDelegate.swift
+++ com.vn.entergroup.Education.gearshop/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+        Thread.sleep(forTimeInterval: 1/2.0)
         //Config firebase
         FirebaseApp.configure()
         
